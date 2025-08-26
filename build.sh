71e37e2a7bfb1914e05bfe88fb92f22a56dc4494
POTAE31121 <tae-b-boy@hotmail.com>
2025-08-26 23:52:58 +0700

    cut command to import employees from CSV

--- build.sh
+++ build.sh
@@ -8,6 +8,6 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 # เพิ่มบรรทัดนี้: (employees.csv คือชื่อไฟล์ที่เราเตรียมไว้)
-python manage.py import_employees employees.csv
+#python manage.py import_employees employees.csv
 
 python manage.py createsuperuser_if_none_exists
